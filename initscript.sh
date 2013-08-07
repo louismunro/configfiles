8ea8e42b305c8a5850901d3f192acb03ddadd70c
Louis Munro <lmunro@inverse.ca>
2013-08-07 15:50:38 -0400

    modified:   .bashalias Added ltr
    modified:   initscript.sh
    Now ignores .git dir and initscript itself.

--- initscript.sh
+++ initscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # a script to create the configuration files and link them to the files in the 
 # repository
-RCFILES=$( ls -da .??* * | grep -v README )
+RCFILES=$( ls -da .??* * | egrep -v -e README -e initscript -e '.git$' )
 for f in $RCFILES; do mv -f  ~/$f{,.original} ; done
 
 for f in $RCFILES; do ln -svf $PWD/$f ~/$f; done
